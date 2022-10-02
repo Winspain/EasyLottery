01bc173dff36106fb62224f4448b7b36af4841d5
winspain <919192088@qq.com>
2022-10-02 23:05:51 +0800

    fix

--- Lottery/easy_lottery/config/gunicorn/production.py
+++ Lottery/easy_lottery/config/gunicorn/production.py
@@ -21,7 +21,7 @@ threads = 2  # 指定每个进程开启的线程数
 loglevel = 'info'  # 日志级别，这个日志级别指的是错误日志的级别，而访问日志的级别无法设置
 access_log_format = '%(t)s %(p)s %(h)s "%(r)s" %(s)s %(L)s %(b)s %(f)s" "%(a)s"'  # 设置gunicorn访问日志格式，错误日志无法设置
 
-
-def child_exit(server, worker):
-    """针对多进程使用prometheus进行适配"""
-    multiprocess.mark_process_dead(worker.pid)
+'''使用flower后则不能使用以下代码,否则celery_flower.out内持续报错'''
+# def child_exit(server, worker):
+#     """针对多进程使用prometheus进行适配"""
+#     multiprocess.mark_process_dead(worker.pid)
