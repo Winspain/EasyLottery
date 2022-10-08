01fe453a4c596f32c4d17656fe00afe046b6ce5a
winspain <919192088@qq.com>
2022-10-08 14:43:00 +0800

    fix:celery task

--- Lottery/apps/lottery/tasks.py
+++ Lottery/apps/lottery/tasks.py
@@ -13,5 +13,5 @@ from celery import shared_task
 @shared_task
 def crawl_by_500():
     url = 'http://127.0.0.1:8888/easyLottery/v1/lottery/craw'
-    response_data = requests.post(url)
-    return response_data
+    response_data = requests.post(url).json()
+    return response_data['data']
