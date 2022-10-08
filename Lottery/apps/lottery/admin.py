cfdbd4e235101943f098abf4db7f926475667062
winspain <919192088@qq.com>
2022-10-08 16:05:53 +0800

    feat:admin.py

--- Lottery/apps/lottery/admin.py
+++ Lottery/apps/lottery/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from lottery.models import LotterySelectInfo
+from lottery.models import LotterySelectInfo, LotteryInfo
 
 
-class LotteryAdmin(admin.ModelAdmin):
-    list_display = ['frontNum1', 'frontNum2', 'frontNum3', 'frontNum4', 'frontNum5', 'backNum1', 'backNum2', 'isDeleted', 'createdTime']
+class LotterySelectAdmin(admin.ModelAdmin):
+    list_display = ['id', 'frontNum1', 'frontNum2', 'frontNum3', 'frontNum4', 'frontNum5', 'backNum1', 'backNum2', 'isDeleted', 'createdTime']
 
 
-admin.site.register(LotterySelectInfo, LotteryAdmin)
+class LotteryInfoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'drawNum', 'frontNum1', 'frontNum2', 'frontNum3', 'frontNum4', 'frontNum5', 'backNum1', 'backNum2', 'drawTime', 'createdTime']
+
+
+admin.site.register(LotteryInfo, LotteryInfoAdmin)
+admin.site.register(LotterySelectInfo, LotterySelectAdmin)
