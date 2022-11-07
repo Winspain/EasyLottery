8610d327f62c8b5f3175d4397e2c68537fc03b56
winspain <919192088@qq.com>
2022-11-07 16:12:34 +0800

    refactor

--- Lottery/apps/lottery/admin.py
+++ Lottery/apps/lottery/admin.py
@@ -7,6 +7,12 @@ from lottery.models import LotterySelectInfo, LotteryInfo, LotteryWebhookInfo
 class LotterySelectAdmin(admin.ModelAdmin):
     list_display = ['id', 'frontNum1', 'frontNum2', 'frontNum3', 'frontNum4', 'frontNum5', 'backNum1', 'backNum2', 'isDeleted', 'createdTime']
 
+    def add_view(self, request, form_url='', extra_context=None):
+        data = request.POST.copy()
+        data['createdBy'] = request.user.id
+        request.POST = data
+        return super(LotterySelectAdmin, self).add_view(request, form_url='', extra_context=extra_context)
+
 
 class LotteryInfoAdmin(admin.ModelAdmin):
     list_display = ['id', 'drawNum', 'frontNum1', 'frontNum2', 'frontNum3', 'frontNum4', 'frontNum5', 'backNum1', 'backNum2', 'drawTime', 'createdTime']
