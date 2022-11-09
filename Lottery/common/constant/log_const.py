78e8e662eb276761809d101c1e151990dc952376
winspain <919192088@qq.com>
2022-11-09 17:54:28 +0800

    feat: add log_const

---
+++ Lottery/common/constant/log_const.py
@@ -0,0 +1,10 @@
+# -- coding utf-8 --
+# @Time     2022/11/9 17:51
+# @Author   dicardo
+# @File     log_const.py
+# @Software PyCharm
+
+import logging
+
+DJANGO_LOGGER = logging.getLogger('django')
+API_LOGGER = logging.getLogger('api')
